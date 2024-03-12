aac29bf637588a07c670232f993ead6713beb571
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-12 23:24:49 +0100

    cree composant  paren and child

---
+++ src/components/ParentComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ParentComponent extends Component {
+  render() {
+    return (
+      <div>ParentComponent</div>
+    )
+  }
+}
+
+export default ParentComponent
