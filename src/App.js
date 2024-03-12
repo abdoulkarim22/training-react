d23e31bd78c4dd02eaf781ce288e2e9a08712e7b
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-13 00:57:11 +0100

    cree function incrémentation décrémentation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-
+        <Counter/>
     </div>
   );
 }
