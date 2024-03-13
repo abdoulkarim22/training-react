cc7bf04405b979d5d5f1c4ba0fb1b7c8ba4904f4
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-13 04:44:40 +0100

    TaskDetailsPage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Counter from './components/Counter';
 import LoginForm from './components/LoginForm';
 import Page from './components/Page';
 import ProfilePage from './components/ProfilePage';
+import TaskDetailsPage from './components/TaskDetailsPage';
 function App() {
+
+
+
+    
   return (
     <div className="App">
-     <ProfilePage/>
+     <TaskDetailsPage/>
     </div>
   );
 }
