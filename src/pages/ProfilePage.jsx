0af10ca2228203c0fe4b8f664aff6971c810f7b4
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-13 13:45:03 +0100

    je utuliser function component

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
 
-class ProfilePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        firstName: 'abdoul',
-        lastName: 'karim',
-        username: 'abdoulkarim42',
-        email: 'abdoulkarim42@gmail.com',
-        bio: 'Lorem ipsum dolor sit amet,',
-    };
-  }
+function ProfilePage() {
 
-  render() {
-  
+  const profileData = {
+    firstName: 'abdoul',
+    lastName: 'karim',
+    username: 'abdoulkarim42',
+    email: 'abdoulkarim42@gmail.com',
+    bio: 'Lorem ipsum dolor sit amet,',
+  };
 
-    return (
-      <div className="profile-page">
-        <h1>Profil de {this.state.username}</h1>
-        <div>
-          <h2>Informations personnelles :</h2>
-          <p>Nom complet: {this.state.firstName} {this.state.Doe}</p>
-          <p>Nom d'utilisateur: {this.state.johndoe123}</p>
-          <p>Email: {this.state.email}</p>
-          <p>Description: {this.state.bio}</p>
-        </div>
-   
+  return (
+    <div className="profile-page">
+      <h1>Profil de {profileData.username}</h1>
+      <div>
+        <h2>Informations personnelles :</h2>
+        <p>Nom complet: {profileData.firstName} {profileData.lastName}</p>
+        <p>Nom d'utilisateur: {profileData.username}</p>
+        <p>Email: {profileData.email}</p>
+        <p>Description: {profileData.bio}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ProfilePage;
